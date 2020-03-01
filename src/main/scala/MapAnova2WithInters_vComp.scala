63236565738cc007d3b1196201e8e52c938a6a90
Antonia Kontaratou <antoniakon1@gmail.com>
2020-03-01 10:42:45 +0000

    change posterior samples estimation and save to disk

--- src/main/scala/MapAnova2WithInters_vComp.scala
+++ src/main/scala/MapAnova2WithInters_vComp.scala
@@ -1,12 +1,21 @@
-import java.io.File
+import java.io.{BufferedWriter, File, FileWriter}
+import java.util.stream.Collectors
 
 import breeze.linalg._
-import breeze.stats.mean
+import com.cibo.evilplot.numeric.Point
+import com.cibo.evilplot.plot.{LinePlot, _}
+import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
 import com.stripe.rainier.compute._
 import com.stripe.rainier.core._
 import com.stripe.rainier.sampler._
+import com.stripe.rainier.notebook._
+import breeze.stats.mean
+import java.io.PrintWriter
+import org.scalacheck.Prop.Exception
 
+import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object MapAnova2WithInters_vComp {
 
@@ -20,7 +29,7 @@ object MapAnova2WithInters_vComp {
    * Process data read from input file
    */
   def dataProcessing(): (Map[(Int, Int), List[Double]], (Array[Double], Array[Int], Array[Int]), Int, Int) = {
-    val data = csvread(new File("/home/antonia/ResultsFromCloud/CompareRainier/040619/withInteractions/simulInter040619.csv"))
+    val data = csvread(new File("/home/antonia/ResultsFromCloud/CompareRainier/040619/withoutInteractions/1M/simulInter040619.csv"))
     val sampleSize = data.rows
     //println(sampleSize)
     val y = data(::, 0).toArray
@@ -71,39 +80,35 @@ object MapAnova2WithInters_vComp {
     val tauE2RV = Gamma(1, 10000).latent
     val sdE2 = sqrtR(Real(1.0) / tauE2RV)
 
-    // Sample tau, estimate sd to be used in sampling from Normal the interaction effects
-    val tauInterRV = Gamma(1, 10000).latent
-    val sdInter = sqrtR(Real(1.0) / tauInterRV)
-
     // Sample tau, estimate sd to be used in sampling from Normal for fitting the model
     val tauDRV = Gamma(1, 10000).latent
     val sdDR = sqrtR(Real(1.0) / tauDRV)
 
-    //Create a Vec[Real] for each main effect and the interaction effects
+    // Sample tau, estimate sd to be used in sampling from Normal the interaction effects
+    val tauInterRV = Gamma(1, 10000).latent
+    val sdInter = sqrtR(Real(1.0) / tauInterRV)
+
+    //Create a Vec[Real] for each effect
     val eff1p = Normal(0, sdE1).latentVec(n1)
     val eff2p = Normal(0, sdE2).latentVec(n2)
-    
-     /**
+    val interEffp = Normal(0, sdInter).latentVec(n1).map(i => Normal(0, sdInter).latentVec(n2))
+
+    /**
      * Uses the imported data in format (y, alpha, beta)
      * 1. Creates a Vec[Real] for columns alpha & beta
      * 2. zips the vecs to one Vec[(Real, Real)] to map over
      * 3. maps over the Vec[(Real, Real)] to create a normal for each observation
      * 4. Builds the model
-     *
-     * Problem: Slow and wrong results
-     * e.g. For HMC(100, 100, 50) time: 380505.616669ms
-     * For HMC(1000, 1000, 100) 6788099.496656ms
      */
     def implementation2(): Model = {
       val eff1Vec = Vec.from(alpha) //1.
       val eff2Vec = Vec.from(beta) //1.
       val eff1VecZipEff2Vec = eff1Vec zip eff2Vec //2.
-      val modeleff1Vec = eff1VecZipEff2Vec.map{case (a,b) => Normal(mu + eff1p(a) + eff2p(b) , sdDR)} //3.
+      val modeleff1Vec = eff1VecZipEff2Vec.map{case (a,b) => Normal(mu + eff1p(a) + eff2p(b) + interEffp(a)(b), sdDR)} //3.
       val vecModel = Model.observe(obs.toList, modeleff1Vec) //4.
       vecModel
     }
-
-
+    
     def time[A](f: => A) = {
       val s = System.nanoTime
       val ret = f
@@ -118,25 +123,51 @@ object MapAnova2WithInters_vComp {
     val leapfrogSteps = 5
     val thinBy = 1
     val trace = time(vecModel.sample(HMC(warmupIters, samplesPerChain, leapfrogSteps)))
+    println("sampling over...")
     val traceThinned = trace.thin(thinBy)
+    val sampNo = samplesPerChain * 4 / thinBy
 
-    var postAlphas = new mutable.ListBuffer[List[Double]]
-    for(i <- 0 until n1){
-      postAlphas += traceThinned.predict(eff1p(i))
+    val postmuTaus = DenseMatrix(DenseVector(traceThinned.predict(mu).toArray), DenseVector(traceThinned.predict(tauDRV).toArray), DenseVector(traceThinned.predict(tauE1RV).toArray), DenseVector(traceThinned.predict(tauE2RV).toArray))
+
+    def predictAll(myTrace: Trace, vc: Vec[Real]) : List[List[Double]] = {
+      vc.map(i => myTrace.predict(i)).toList
     }
 
-    val postBetas = new mutable.ListBuffer[List[Double]]
-    for(i <- 0 until n2){
-      postBetas += traceThinned.predict(eff2p(i))
-    }
+    def printSamplesToCsv(filename: String, eff: List[List[Double]]): Unit = {
+      val builder = new StringBuilder()
 
-    val postmuTaus = Array(traceThinned.predict(mu).toArray, traceThinned.predict(tauDRV).toArray, traceThinned.predict(tauE1RV).toArray, traceThinned.predict(tauE2RV).toArray)
+      val pw = new PrintWriter(new File(filename))
+
+      eff.transpose.foreach(line => {
+        line.foreach(double => {
+          builder.append(double.toString)
+          builder.append(",")
+        })
+        builder.deleteCharAt(builder.size-1) //delete last comma in line
+        builder.append("\n")
+
+        pw.append(builder) //a. Write at each line
+        pw.flush() //a.
+        builder.clear() //a.
+      })
+
+      //      pw.print(builder) //Or b. write at the end the whole thing
+      pw.close()
+    }
+    val postAlphasN = predictAll(traceThinned, eff1p)
+    println("ddd")
+    println(postAlphasN)
+    val added = List(traceThinned.predict(mu), traceThinned.predict(tauDRV))
+    printSamplesToCsv("/home/antonia/ResultsFromCloud/CompareRainier/040619/withoutInteractions/1M/alphasOnly.csv", postAlphasN)
 
     println("mu, taus")
-    println(postmuTaus.map(el=> mean(el)).toList)
+    println(mean(postmuTaus(::, *)))
     println("alphas")
-    println(postAlphas.map(el=> el.sum/el.size))
+    println(postAlphasN.map(el=>el.sum/el.size))
     println("betas")
-    println(postBetas.map(el=> el.sum/el.size))
+    //println(mean(dmBetas(::, *)))
+
+
   }
 }
+
